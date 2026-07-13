52dd285202bf99a554516a7cdac7782ffe54beeb
Lukasavicus <8000051+Lukasavicus@users.noreply.github.com>
2026-07-13 12:04:22 -0300

    feat: toggle login signup form

--- public/js/app-es6/boot_home.js
+++ public/js/app-es6/boot_home.js
@@ -1,34 +1,52 @@
 import {loginControllerInstance} from './controllers/LoginController';
 
 let loginController = loginControllerInstance();
+let homeForm = document.querySelector("#home-form");
+let homeFormTitle = document.querySelector("#home-form-title");
 let homeMessage = document.querySelector("#home-message");
 
-document.querySelector("#login-btn").addEventListener('click',
+function showSignup(event) {
+    event.preventDefault();
+    homeMessage.innerText = "";
+    homeForm.classList.add("signup-mode");
+    homeFormTitle.innerText = "Create account";
+}
+
+function showLogin(event) {
+    event.preventDefault();
+    homeMessage.innerText = "";
+    homeForm.classList.remove("signup-mode");
+    homeFormTitle.innerText = "Welcome back";
+}
+
+document.querySelector("#show-signup").addEventListener('click', showSignup);
+document.querySelector("#show-login").addEventListener('click', showLogin);
+
+homeForm.addEventListener('submit',
     function(event){
         event.preventDefault();
+
+        if(homeForm.classList.contains("signup-mode")) {
+            homeMessage.innerText = "";
+
+            try {
+                loginController.signup(
+                    document.querySelector("#signup-login").value,
+                    document.querySelector("#signup-email").value,
+                    document.querySelector("#signup-password").value
+                )
+                .catch(error => homeMessage.innerText = error.message || "Could not create account");
+            }
+            catch(error) {
+                homeMessage.innerText = error.message;
+            }
+
+            return false;
+        }
+
         const login_ =  document.querySelector("#login").value;
         const pass_ = document.querySelector("#password").value;
         //console.log(login_, pass_);
         loginController.login(login_, pass_);
-        return false;
-});
-
-document.querySelector("#signup-btn").addEventListener('click',
-    function(event){
-        event.preventDefault();
-        homeMessage.innerText = "";
-
-        try {
-            loginController.signup(
-                document.querySelector("#signup-login").value,
-                document.querySelector("#signup-email").value,
-                document.querySelector("#signup-password").value
-            )
-            .catch(error => homeMessage.innerText = error.message || "Could not create account");
-        }
-        catch(error) {
-            homeMessage.innerText = error.message;
-        }
-
         return false;
 });
--- public/js/app/boot_home.js
+++ public/js/app/boot_home.js
@@ -3,37 +3,56 @@
 System.register(["./controllers/LoginController"], function (_export, _context) {
     "use strict";
 
-    var loginControllerInstance, loginController, homeMessage;
+    var loginControllerInstance, loginController, homeForm, homeFormTitle, homeMessage;
+
+
+    function showSignup(event) {
+        event.preventDefault();
+        homeMessage.innerText = "";
+        homeForm.classList.add("signup-mode");
+        homeFormTitle.innerText = "Create account";
+    }
+
+    function showLogin(event) {
+        event.preventDefault();
+        homeMessage.innerText = "";
+        homeForm.classList.remove("signup-mode");
+        homeFormTitle.innerText = "Welcome back";
+    }
+
     return {
         setters: [function (_controllersLoginController) {
             loginControllerInstance = _controllersLoginController.loginControllerInstance;
         }],
         execute: function () {
             loginController = loginControllerInstance();
+            homeForm = document.querySelector("#home-form");
+            homeFormTitle = document.querySelector("#home-form-title");
             homeMessage = document.querySelector("#home-message");
+            document.querySelector("#show-signup").addEventListener('click', showSignup);
+            document.querySelector("#show-login").addEventListener('click', showLogin);
 
-
-            document.querySelector("#login-btn").addEventListener('click', function (event) {
+            homeForm.addEventListener('submit', function (event) {
                 event.preventDefault();
+
+                if (homeForm.classList.contains("signup-mode")) {
+                    homeMessage.innerText = "";
+
+                    try {
+                        loginController.signup(document.querySelector("#signup-login").value, document.querySelector("#signup-email").value, document.querySelector("#signup-password").value).catch(function (error) {
+                            return homeMessage.innerText = error.message || "Could not create account";
+                        });
+                    } catch (error) {
+                        homeMessage.innerText = error.message;
+                    }
+
+                    return false;
+                }
+
                 var login_ = document.querySelector("#login").value;
                 var pass_ = document.querySelector("#password").value;
                 //console.log(login_, pass_);
                 loginController.login(login_, pass_);
-                return false;
-            });
-
-            document.querySelector("#signup-btn").addEventListener('click', function (event) {
-                event.preventDefault();
-                homeMessage.innerText = "";
-
-                try {
-                    loginController.signup(document.querySelector("#signup-login").value, document.querySelector("#signup-email").value, document.querySelector("#signup-password").value).catch(function (error) {
-                        return homeMessage.innerText = error.message || "Could not create account";
-                    });
-                } catch (error) {
-                    homeMessage.innerText = error.message;
-                }
-
                 return false;
             });
         }
